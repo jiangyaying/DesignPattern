94025fa1a0c48215514a726f2ca8474d2b984a59
jiangyaying <547882479@qq.com>
2020-04-12 17:51:37 +0800

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by jiang on 2020/4/12.
+//  Copyright © 2020 jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
